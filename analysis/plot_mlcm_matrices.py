d42f93e8500bb273002a45b40fd24785dc0c5482
Guillermo Aguilar <gmo.aguilar.c@gmail.com>
2023-02-22 18:35:39 +0100

    luminance measurmentes analysed

--- analysis/plot_mlcm_matrices.py
+++ analysis/plot_mlcm_matrices.py
@@ -18,55 +18,57 @@ sns.set_context('talk')
 
 name = 'ga6'
 
-# %% MLCM results
-### loading raw file.
-d = pd.read_csv("../data/parsed_results/%s_reindexed_full.csv" % name)
-
-# luminances
-lums = pd.concat((d['lum1'], d['lum2'])).unique()
-lums.sort()
+for name in names:
     
-# contexts
-carriers = [0, 1]
-
-stimlist = [(l,c) for c in carriers for l in lums ]
-
-
-data = np.zeros((20, 20))
-mask = np.zeros((20, 20))
-Ns = np.zeros((20, 20))
-
-for i in range(20):
-    for j in range(20):
+    # %% MLCM results
+    ### loading raw file.
+    d = pd.read_csv("../data/parsed_results/%s_reindexed_full.csv" % name)
+    
+    # luminances
+    lums = pd.concat((d['lum1'], d['lum2'])).unique()
+    lums.sort()
         
-        s_row = stimlist[i]
-        s_col = stimlist[j]
-        
-        set1 = d[(d['lum1']==s_row[0]) &( d['carrier1']==s_row[1]) & (d['lum2']==s_col[0]) &( d['carrier2']==s_col[1])]
-        
-        
-        set2 = d[(d['lum1']==s_col[0]) &( d['carrier1']==s_col[1]) & (d['lum2']==s_row[0]) &( d['carrier2']==s_row[1])]
-        
-        N = len(set1) + len(set2)
-        
-        if N>0:
-            n = np.sum(set2['Resp'].values) + np.sum(np.logical_not(set1['Resp'].values).astype(int))
-            data[i,j] = n / N
-            Ns[i,j] = N
+    # contexts
+    carriers = [0, 1]
+    
+    stimlist = [(l,c) for c in carriers for l in lums ]
+    
+    
+    data = np.zeros((20, 20))
+    mask = np.zeros((20, 20))
+    Ns = np.zeros((20, 20))
+    
+    for i in range(20):
+        for j in range(20):
             
-        else:
-            data[i,j] = np.nan
-        
-        if s_row[1] == s_col[1] and s_row[0] > s_col[0]:
-            mask[i,j] = 1
-        elif s_row[1] > s_col[1]:
-            mask[i,j] = 1
-
-# %%
-plt.figure(figsize=(25, 20))
-sns.heatmap(data, mask=mask, annot=True, xticklabels=stimlist, yticklabels=stimlist)
-plt.title('%s - N per cell = %d' % (name, Ns.max()))
-plt.savefig('../figs/%s_resp_matrix.pdf' % name)
+            s_row = stimlist[i]
+            s_col = stimlist[j]
+            
+            set1 = d[(d['lum1']==s_row[0]) &( d['carrier1']==s_row[1]) & (d['lum2']==s_col[0]) &( d['carrier2']==s_col[1])]
+            
+            
+            set2 = d[(d['lum1']==s_col[0]) &( d['carrier1']==s_col[1]) & (d['lum2']==s_row[0]) &( d['carrier2']==s_row[1])]
+            
+            N = len(set1) + len(set2)
+            
+            if N>0:
+                n = np.sum(set2['Resp'].values) + np.sum(np.logical_not(set1['Resp'].values).astype(int))
+                data[i,j] = n / N
+                Ns[i,j] = N
+                
+            else:
+                data[i,j] = np.nan
+            
+            if s_row[1] == s_col[1] and s_row[0] > s_col[0]:
+                mask[i,j] = 1
+            elif s_row[1] > s_col[1]:
+                mask[i,j] = 1
+    
+    # %%
+    plt.figure(figsize=(25, 20))
+    sns.heatmap(data, mask=mask, annot=True, xticklabels=stimlist, yticklabels=stimlist)
+    plt.title('%s - N per cell = %d' % (name, Ns.max()))
+    plt.savefig('../figs/%s_resp_matrix.pdf' % name)
 
 
 
