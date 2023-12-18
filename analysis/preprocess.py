ed994d64f808dbe5836410123c6c346765af772d
Joris Vincent <joris.vincent@tu-berlin.de>
2023-12-18 19:37:10 +0100

    fix(preprocess): create "analyzed" subdir if not already exists

--- analysis/preprocess.py
+++ analysis/preprocess.py
@@ -83,8 +83,11 @@ if __name__ == "__main__":
         # i.e., if we use the same variables for all blocks/stimuli.
         # In the current experimental setup, that should be the case.
         merged = merge_results(data_management.participant_results[participant])
+        filepath = data_management.results_dir / participant / "analyzed" / f"{participant}.csv"
+        if not filepath.parent.exists():
+            filepath.parent.mkdir(parents=True)
         merged.to_csv(
-            data_management.results_dir / participant / "analyzed" / f"{participant}.csv",
+            filepath,
             sep=",",
             index=False,
         )
