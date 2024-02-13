f60ba1cba4a007a7aea4e83aa0917ccb7edfa3c8
Joris Vincent <joris.vincent@tu-berlin.de>
2024-02-13 15:30:56 +0100

    feat(postprocess): reindex, and overwrite scales
    for single participant

--- analysis/postprocess.py
+++ analysis/postprocess.py
@@ -1,3 +1,6 @@
+import pandas as pd
+
+from surround_brightness import data_management
 from surround_brightness.analysis.preprocess import CONTEXTS_TO_IDC
 from surround_brightness.experiment.design import intensities as exp_intensities
 
@@ -15,3 +18,16 @@ def reindex_scales(scales):
     scales["intensity"] = scales["intensity"].replace(ints).astype(float)
 
     return scales
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    participant = "DEMO"
+    stim = "whites"
+    modeltype = "full"
+    filename = f"{participant}_{stim}_{modeltype}_trimmed_{modeltype}_norm.scales.csv"
+    filepath = data_management.results_dir / participant / "analyzed" / filename
+
+    s = reindex_scales(pd.read_csv(filepath, sep=","))
+    s.to_csv(filepath, sep=",", index=False)
