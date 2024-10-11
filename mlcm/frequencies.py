50fac2985f7b42198a1073bc947fc640108a3997
Joris Vincent <joris.vincent@tu-berlin.de>
2024-10-11 15:37:05 +0200

    feat: conjoint frequencies

--- mlcm/frequencies.py
+++ mlcm/frequencies.py
@@ -1,7 +1,63 @@
-# Imports
+import numpy as np
+import pandas as pd
+
+from . import utils
 
 
-def conjoint_frequencies(): ...
+def conjoint(trials, col, dim_names=("dim1", "dim2"), pair_names=("A", "B")):
+    """Conjoint proportion/frequencies/total per unique stimuli pairing, for given column
+
+    Creates a pivot table, where for the specified `col`umn, the aggregated total
+    is shown for every possible stimuli pairing (e.g. every dimensions x pair combinations).
+
+    This is useful e.g., to show the number of repetitions set for each stimulus pairing,
+    or choice frequencies (see conjoint_choice).
+
+    For stimuli pairings not present in the data, will show NaN.
+
+    Parameters
+    ----------
+    trials : pandas.DataFrame
+        raw trials response data, with one column per dimension x pair combination
+        e.g., "dim1_A", "dim2_A", "dim1_B", "dim2_B"
+    col : str
+        name of column (in trials) to calculate conjoint total for
+    dim_names : tuple[str], optional
+        names for the stimulus dimensions, by default ("dim1", "dim2")
+    pair_names : tuple[str], optional
+        names for the stimulus pair members, by default ("A", "B")
+
+    Returns
+    -------
+    pandas.DataFrame
+        pivot table of conjoint frequencies,
+        where each row and each column is unique dimension-level combination (unique stimulus),
+        and thus each cell represents a unique stimuli pairing
+        (where order matters, i.e., ('A', 'B') != ('B', 'A') )
+    """
+
+    # Calculate frequencies (pivot table, summing over specified column)
+    freqs = trials.pivot_table(
+        index=[f"{dim_names[1]}_{pair_names[0]}", f"{dim_names[0]}_{pair_names[0]}"],
+        columns=[f"{dim_names[1]}_{pair_names[1]}", f"{dim_names[0]}_{pair_names[1]}"],
+        values=col,
+        aggfunc="sum",
+    )
+
+    # Housekeeping: format dataframe
+    # Construct all possible combinations, to fill out pivot table
+    stim_levels = utils.extract_stim_levels(trials, dim_names=dim_names, pair_names=pair_names)
+    multiindex_full = utils.dimension_combinations(stim_levels)
+    # Fill out and sort indices
+    freqs = (
+        freqs.sort_index(axis="columns", level=[0, 1], ascending=True)
+        .reindex(multiindex_full.copy(), fill_value=np.nan)
+        .reindex(multiindex_full.copy(), fill_value=np.nan, axis="columns")
+    )
+    # Label indices
+    freqs.index.name, freqs.columns.name = pair_names
+
+    return freqs
 
 
 def unmirror_frequencies(): ...
--- tests/test_frequencies.py
+++ tests/test_frequencies.py
@@ -1,3 +1,53 @@
+import numpy as np
+import pandas as pd
+
+from mlcm import frequencies
+
+
+def test_conjoint():
+    trials = pd.DataFrame(
+        {
+            "dim1_A": [1, 2, 3],
+            "dim2_A": ["X", "Y", "X"],
+            "dim1_B": [2, 3, 1],
+            "dim2_B": ["Y", "Y", "X"],
+            "repeats": ["3", "2", "4"],
+        }
+    )
+
+    freqs = frequencies.conjoint(trials, col="repeats")
+
+    assert [freqs.index.name, freqs.columns.name] == ["A", "B"]
+    assert freqs.index.names == freqs.columns.names == ["dim2", "dim1"]
+    assert (
+        list(freqs.columns)
+        == list(freqs.index)
+        == [
+            ("X", 1),
+            ("X", 2),
+            ("X", 3),
+            ("Y", 1),
+            ("Y", 2),
+            ("Y", 3),
+        ]
+    )
+
+    assert np.array_equal(
+        freqs.values.astype(float),
+        np.array(
+            [
+                [np.nan, np.nan, np.nan, np.nan, 3, np.nan],
+                [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+                [4, np.nan, np.nan, np.nan, np.nan, np.nan],
+                [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+                [np.nan, np.nan, np.nan, np.nan, np.nan, 2],
+                [np.nan, np.nan, np.nan, np.nan, np.nan, np.nan],
+            ],
+        ),
+        equal_nan=True,
+    )
+
+
 def test_frequencies(): ...
 
 
