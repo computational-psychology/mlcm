bc566ba965ba84a0fab8f8b085617c5b89d00d10
Joris Vincent <joris.vincent@tu-berlin.de>
2024-11-19 17:06:45 +0100

    test(utils): `extract_stim_levels` can handle >2 dimensions

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,6 +22,28 @@ def test_extract_stim_levels():
     assert all(stim_levels["dimY"] == ("X", "Y"))
 
 
+def test_extract_3_dim_levels():
+    trials = {
+        "dimX_left": [1, 2, 3],
+        "dimY_left": ["X", "Y", "X"],
+        "dimZ_left": [0.5, 5.0, 5.0],
+        "dimX_right": [2, 3, 1],
+        "dimY_right": ["Y", "Y", "X"],
+        "dimZ_right": [2.0, 0.5, 2.0],
+        "response": ["left", "right", "left"],
+    }
+    trials = pd.DataFrame(trials)
+
+    stim_levels = utils.extract_stim_levels(
+        trials, dim_names=("dimX", "dimY", "dimZ"), pair_names=("left", "right")
+    )
+
+    assert tuple(stim_levels.keys()) == ("dimX", "dimY", "dimZ")
+    assert all(stim_levels["dimX"] == (1, 2, 3))
+    assert all(stim_levels["dimY"] == ("X", "Y"))
+    assert all(stim_levels["dimZ"] == (0.5, 2.0, 5.0))
+
+
 def test_dimension_combinations():
     stim_levels = {"dimY": ["X", "Y"], "dimX": [1, 2, 3]}
 
