57060a7ff262daff86b62ed0ac19e3c87060f880
Joris Vincent <joris.vincent@tu-berlin.de>
2026-06-11 12:44:49 +0200

    test(datasets): add bootstrapped CIs to test output

--- tests/example_datasets/test_GlossyBumpy.R
+++ tests/example_datasets/test_GlossyBumpy.R
@@ -19,11 +19,27 @@ write.csv(
 # Fit the MLCM model
 model <- mlcm(df, model = "full")
 
+# Run bootstrap
+set.seed(123)
+bootstrap <- boot.mlcm(model, nsim = 1000)
+samples <- bootstrap$boot.samp
+
+# Calculate for each row (each parameter) the percentiles
+bootstrap$CI_low <- c(0, apply(samples, 1, quantile, probs = 0.025))
+bootstrap$CI_high <- c(0, apply(samples, 1, quantile, probs = 0.975))
+
+# Reformat to same shape as scale values
+dim(bootstrap$CI_low) <- dim(model$pscale)
+dim(bootstrap$CI_high) <- dim(model$pscale)
+
+
 # Long format: G, B, pscale
 scales_long <- data.frame(
   glossiness = rep(seq_len(nrow(model$pscale)), each = ncol(model$pscale)),
   bumpiness = rep(seq_len(ncol(model$pscale)), times = nrow(model$pscale)),
-  scale = as.vector(t(model$pscale))
+  scale = as.vector(t(model$pscale)),
+  CI_low = as.vector(t(bootstrap$CI_low)),
+  CI_high = as.vector(t(bootstrap$CI_high))
 )
 
 # Sort by B then G
